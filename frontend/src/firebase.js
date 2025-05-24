c90ec14efda0efe4c85d209406abb313142b05d4
Taha kanchwala <info.tahakanch@gmail.com>
2025-05-24 15:00:15 +0530

    replace the firebase config with dynamic variable

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcunepln_fvK_tedBLTZRPbcPRlV--1wY",
-  authDomain: "test-app-bd9a9.firebaseapp.com",
-  projectId: "test-app-bd9a9",
-  storageBucket: "test-app-bd9a9.firebasestorage.app",
-  messagingSenderId: "790085495352",
-  appId: "1:790085495352:web:3eeda384798dca57b794cd",
-  measurementId: "G-NCFX251CR4"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
